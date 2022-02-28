c9aa372f1da0b018db48ba07090998dfaf769e3e
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-02-28 13:15:10 +0100

    Tidies up code

--- ui.R
+++ ui.R
@@ -1,17 +1,5 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-if (!require(shinyWidgets)) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
-}
+library(shinyWidgets)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
